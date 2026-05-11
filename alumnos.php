acf5c23ec227ccfdd29de9fdcccc8726ce8dde57
jalburgas <juanjalbur@gmail.com>
2026-05-11 10:25:08 -0400

    actualizar

--- alumnos.php
+++ alumnos.php
@@ -3,11 +3,13 @@
 ?>
 
 <html>
+
 <head>
     <title>PHP Test</title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="estilos.css">
 </head>
+
 <body>
     <div class="container mt-5">
         <form method="post">
@@ -17,7 +19,7 @@
                     <a href="menu.php" class="btn btn-secondary">← Regresar al Menú</a>
                 </div>
             </div>
-            
+
             <div class="mb-3">
                 <div class="card" style="width: 18rem;">
                     <p class="card-header">
@@ -25,18 +27,19 @@
                     </p>
                     <div class="card-body">
                         <input type="text" name="cedula" class="form-control" placeholder="Cedula" required>
-                        <br>                
+                        <br>
                         <input type="text" name="nombre" class="form-control" placeholder="Nombre" required>
                         <br>
                         <input type="number" name="edad" class="form-control" placeholder="Edad" required>
                     </div>
                 </div>
             </div>
-            
+
             <button type="submit" class="btn btn-primary">Registrar</button>
         </form>
     </div>
 
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
